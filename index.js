b4f944109894880df4dc1b0fef2a2967ca71eb21
Clarence Kwong <clarencekwong7@gmail.com>
2018-06-27 20:37:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -120,10 +120,10 @@ function createRock(x) {
  * Finally, alert "YOU LOSE!" to the player.
  */
 function endGame() {
-  clearInterval(gameInterval)
+  clearInterval(gameInterval);
   ROCKS.forEach(function(rock) {
-    
-  }) 
+    rock.remove();
+  });
 }
 
 function moveDodger(e) {
